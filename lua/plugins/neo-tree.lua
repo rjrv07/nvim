692d33e013800c58395eeb7a409649084a090355
rjrv07 <github@roelie.co.za>
2026-04-23 12:50:29 +0200

    add neo-tree keybind

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,12 +1,13 @@
 return {
-        {
-                "nvim-neo-tree/neo-tree.nvim",
-                branch = "v3.x",
-                dependencies = {
-                        "nvim-lua/plenary.nvim",
-                        "MunifTanjim/nui.nvim",
-                        "nvim-tree/nvim-web-devicons", 
-                },
-                lazy = false, 
-        }
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+                "nvim-lua/plenary.nvim",
+                "MunifTanjim/nui.nvim",
+                "nvim-tree/nvim-web-devicons", 
+        },
+        lazy = false, 
+        opts = function()
+                vim.keymap.set("n", "<leader>n", ":Neotree toggle<CR>")
+        end,
 }
