c4357b0311bfa8127b64093d4fb68011a45c7123
rjrv07 <github@roelie.co.za>
2026-04-23 19:08:59 +0200

    created custom splash screen

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -2,6 +2,22 @@ return {
 	"nvimdev/dashboard-nvim",
 	event = "VimEnter",
 	dependencies = { { "nvim-tree/nvim-web-devicons" } },
-        opts = {
-        },
+	opts = {
+		config = {
+			header = {
+				" ,ggggggggggg,    ,gggggggggggggggggg,    ,ggg,         ,gg                       ",
+				'dP"""""""""""Y8, dP""""88""""""""""""Y8, dP""Y8a       ,8P                        ',
+				"Yb,  gg      `8b Yb,   88    gg      `8b Yb, `88       d8'                        ",
+				' `"  88      ,8P  `"   88    88      ,8P  `"  88       88  gg                     ',
+				'     88aaaad8P"        88    88aaaad8P"       88       88  ""                     ',
+				'     88""""Yb,         8I    88""""Yb,        I8       8I  gg    ,ggg,,ggg,,ggg,  ',
+				'     88     "8b       ,8\'    88     "8b       `8,     ,8\'  88   ,8" "8P" "8P" "8, ',
+				"    ,8P      `8i     ,8P    ,8P      `8i       Y8,   ,8P   88   I8   8I   8I   8I ",
+				"    \"'        Yb,___,dP     \"'        Yb,       Yb,_,dP  _,88,_,dP   8I   8I   Yb,",
+				'               "Y888P"                `Y8        "Y8P"   8P""Y88P\'   8I   8I   `Y8',
+			},
+			shortcut = {},
+			footer = {},
+		},
+	},
 }
