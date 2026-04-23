d9915588d87c80acfa1ebb6f76017f2f0373f5da
rjrv07 <github@roelie.co.za>
2026-04-23 13:29:15 +0200

    restructure vim options

--- init.lua
+++ init.lua
@@ -1,8 +1,2 @@
 require("config.lazy")
-
-vim.opt.expandtab = true
-vim.opt.tabstop = 8
-vim.opt.softtabstop = 8
-vim.opt.shiftwidth = 8
-
-vim.cmd.colorscheme "catppuccin-mocha"
+require("vim-options")
---
+++ lua/vim-options.lua
@@ -0,0 +1,6 @@
+vim.opt.expandtab = true
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 8
+vim.opt.shiftwidth = 8
+
+vim.cmd.colorscheme "catppuccin-mocha"
