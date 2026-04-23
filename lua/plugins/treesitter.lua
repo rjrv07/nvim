6ccf99297d575aa67ec31928b964d818704477b1
rjrv07 <github@roelie.co.za>
2026-04-23 12:20:48 +0200

    fix treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,13 +2,15 @@ return {
         "nvim-treesitter/nvim-treesitter",
         lazy = false,
         build = ":TSUpdate",
-        opts = function()
-                require("nvim-treesitter").install {
-                        "lua",
-                        "cpp",
-                        "cmake",
-                        "haskell",
-                        "python",
-                }
-        end
+        config = function()
+                require("nvim-treesitter").install({ "lua", "cpp", "cmake", "haskell", "python" })
+                vim.api.nvim_create_autocmd("FileType", {
+                        callback = function()
+                                local ok = pcall(vim.treesitter.start)
+                                if ok then
+                                        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                                end
+                        end,
+                })
+        end,
 }
