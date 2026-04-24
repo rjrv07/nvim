f0b95566a7a9d254a7982d9361c2c1b1b6e85129
rjrv07 <github@roelie.co.za>
2026-04-24 09:14:39 +0200

    change clangd default formatting to match nvim settings

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,12 +1,42 @@
 return {
 	"mason-org/mason-lspconfig.nvim",
-	opts = {
-		ensure_installed = { "lua_ls", "stylua", "clangd", "hls", "pyright", "neocmake" },
-	},
 	dependencies = {
 		{ "mason-org/mason.nvim", opts = {} },
 		"neovim/nvim-lspconfig",
 	},
+	opts = {
+		ensure_installed = { "lua_ls", "stylua", "clangd", "hls", "pyright", "neocmake" },
+
+		handlers = {
+			function(server_name)
+				require("lspconfig")[server_name].setup({})
+			end,
+			["clangd"] = function()
+				require("lspconfig").clangd.setup({
+					cmd = {
+						"clangd",
+						"--fallback-style=" .. vim.fn.json_encode({
+							BasedOnStyle = "LLVM",
+							IndentWidth = 8,
+							TabWidth = 8,
+							UseTab = "Never",
+						}),
+					},
+				})
+			end,
+		},
+	},
+	config = function(_, opts)
+		require("mason-lspconfig").setup(opts)
+
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function()
+				vim.lsp.buf.format({ async = false })
+			end,
+			desc = "LSP auto-format on save",
+		})
+	end,
 	keys = {
 		{ "K", vim.lsp.buf.hover, { desc = "Show LSP hover info" } },
 		{ "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP code action" } },
