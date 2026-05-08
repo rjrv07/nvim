bb4f6da9befed70229a000552d553d4f69e56c55
rjrv07 <github@roelie.co.za>
2026-05-08 09:25:05 +0200

    set scrolloff

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -9,6 +9,8 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
 
+vim.opt.scrolloff = 10
+
 vim.opt.completeopt = "fuzzy,menuone,noselect,popup"
 vim.opt.autocomplete = true
 vim.opt.pumheight = 7
